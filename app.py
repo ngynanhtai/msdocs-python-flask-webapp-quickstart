a2aebd8813b89225958b7b93777156c0d6672e05
ngynanhtai <ngynanhtai@gmail.com>
2024-10-17 21:03:04 +0700

    update project

--- app.py
+++ app.py
@@ -1,32 +1,50 @@
-import os
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+import pandas as pd
+# from sklearn.externals import joblib
+import joblib
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
 
+def scale(payload):
+    """Scales Payload"""
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    LOG.info("Scaling Payload: %s payload")
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route("/")
+def home():
+    html = "<h3>Sklearn Prediction Home</h3>"
+    return html.format(format)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+# TO DO:  Log out the prediction value
+@app.route("/predict", methods=['POST'])
+def predict():
+    # Performs an sklearn prediction
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    try:
+        # Load pretrained model as clf. Try any one model. 
+        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
+        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+    except:
+        LOG.info("JSON payload: %s json_payload")
+        return "Model not loaded"
 
+    json_payload = request.json
+    LOG.info("JSON payload: %s json_payload")
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info("inference payload DataFrame: %s inference_payload")
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
+    return jsonify({'prediction': prediction})
 
-if __name__ == '__main__':
-   app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
